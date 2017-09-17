a9d7f5a5354b6f6109b24bf837fcf4a20ca3b219
LimHoSung <wonder789@naver.com>
2017-09-17 22:33:11 +0900

    fillRemainWidth 옵션 추가

--- src/directives/spGridHeader.js
+++ src/directives/spGridHeader.js
@@ -14,10 +14,16 @@ function spGridHeader( $compile, SpGridConstant){
             var _useageWidth = 0;
 
             var _headerColumns = scope.gridObject.getColumnDef();
+            var _wildCardColumn = null;
 
             angular.forEach( _headerColumns, function( column ){
                 if( column.hasOwnProperty("width") ){
-                    _useageWidth += parseInt(column.width);
+                    if( column.hasOwnProperty("fillRemainWidth")
+                        && column.fillRemainWidth ){
+                        _wildCardColumn = column;
+                    } else {
+                        _useageWidth += parseInt(column.width);
+                    }
                 } else {
                     throw new Error([ column.name + "컬럼의 너비를 지정해주세요"])
                 }
@@ -25,13 +31,19 @@ function spGridHeader( $compile, SpGridConstant){
 
             var _gapWidth = (_headerWidth - _useageWidth)-5;
 
-            if( _gapWidth > 0 ){
-                var _usePercentage = 0;
-                angular.forEach( _headerColumns, function( column ){
-                    _usePercentage = parseInt(column.width) / _useageWidth;
-                    column.width = (parseInt(column.width) + parseFloat(( _usePercentage * _gapWidth ).toFixed(1))) + "px";
-                    console.log(parseFloat(( _usePercentage * _gapWidth ).toFixed(1)));
-                });
+            if( _wildCardColumn == null ){
+                if( _gapWidth > 0 ){
+                    var _usePercentage = 0;
+                    angular.forEach( _headerColumns, function( column ){
+                        _usePercentage = parseInt(column.width) / _useageWidth;
+                        column.width = (parseInt(column.width) + parseFloat(( _usePercentage * _gapWidth ).toFixed(1))) + "px";
+                        console.log(parseFloat(( _usePercentage * _gapWidth ).toFixed(1)));
+                    });
+                }
+            } else {
+                if( _gapWidth > 0 ){
+                    _wildCardColumn.width = (_gapWidth+5) + "px";
+                }
             }
 
         }
