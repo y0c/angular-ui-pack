6e5bc4962e5ea4d07d302d574c060c384011f2ec
LimHoSung <wonder789@naver.com>
2017-09-25 02:05:41 +0900

    Modal 개발중.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/modal",function(req,res,next){
+  res.render("modal");
+});
 module.exports = router;
