3875e0e4f740099d48ba66ea7ec1235fe330249a
LimHoSung <wonder789@naver.com>
2018-04-13 11:57:17 +0900

    Seperate SpUi.js SpModal.css

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 require("./SpGrid/app.js");
 require("./SpTab/app.js");
 require("./SpUtil/app.js");
-require("../public/less/SpModal.less");
 
 var app = angular.module("SpUi", ["SpGrid", "SpTab", "SpUtil"]);
 
