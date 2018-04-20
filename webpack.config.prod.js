e4eba63011fff8db893ec3639e80032eecb73785
LimHoSung <wonder789@naver.com>
2018-04-20 23:27:41 +0900

    webpack 설정 분리 및 스크립트 추가

---
+++ webpack.config.prod.js
@@ -0,0 +1,51 @@
+var webpack = require("webpack");
+const path = require("path");
+const config = {
+    entry: [
+        './src/app.js'
+    ],
+    output: {
+        path: path.resolve(__dirname) + '/public/javascripts/dist',
+        filename: 'SpUi.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module :{
+        rules: [
+        {
+            test: /\.html$/,
+            use: [ {
+                loader: 'html-loader',
+                options: {
+                    minimize: true
+                }
+            }]
+        },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+            { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            }
+
+        ]
+    },
+    // devtool: '#inline-source-map'
+};
+
+module.exports = config;
